fe16b48045c1b8e9e783f0dbce5dafe48e6f804a
zsy-code <zdm.code@gmail.com>
2024-08-09 10:51:43 +0800

    update

--- code/transformer/layers.py
+++ code/transformer/layers.py
@@ -11,20 +11,19 @@ class PositionalEncoding(nn.Module):
         super(PositionalEncoding, self).__init__()
         self.d_model = d_model
         
-        # 创建一个位置编码矩阵
+        # create a constant 'pe' matrix with the (1, n_positions, d_model) shape
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        x = x + self.pe[:x.size(0), :]
+        x = x + self.pe[:, :x.size(1)]
         return x
-
-
+    
 
 class PositionwiseFeedForward(nn.Module):
     """ The Position Wise Feed Forward Layer in Transformer
